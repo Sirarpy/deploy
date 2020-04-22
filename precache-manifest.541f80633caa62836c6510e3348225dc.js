bb263995ee59ab547e3750944b85a1b3cbb139a1
Sirarpy <sipadavtyan@gmail.com>
2020-04-22 16:47:27 +0400

    Updates

---
+++ precache-manifest.541f80633caa62836c6510e3348225dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f6d36ac78ea15fea990e9a5b2ac494",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "02ed00f10ba493dc2519",
+    "url": "/deploy/static/css/main.de193855.chunk.css"
+  },
+  {
+    "revision": "f782e574c640d2c8965a",
+    "url": "/deploy/static/js/2.8d1aa7bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy/static/js/2.8d1aa7bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ed00f10ba493dc2519",
+    "url": "/deploy/static/js/main.30cc26eb.chunk.js"
+  },
+  {
+    "revision": "ba41e8d6378eb9484438",
+    "url": "/deploy/static/js/runtime-main.06852667.js"
+  },
+  {
+    "revision": "22256a70e83d01b3e60769871876f204",
+    "url": "/deploy/static/media/back.22256a70.jpg"
+  }
+]);
